5106eba467ad74115fa46dd5ca2d5b3a3982d16c
liuzhe <liu55zhe@163.com>
2020-07-25 16:44:14 +0800

    测试接口

---
+++ src/api/login.js
@@ -0,0 +1,16 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/admin/login/check',
+    method: 'post',
+    data
+  })
+}
+export function index(data) {
+  return request({
+    url: 'admin/news/index',
+    method: 'post',
+    data
+  })
+}
